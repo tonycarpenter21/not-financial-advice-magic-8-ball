a8233242f91f7ec7951c7182a15b578a1f68512d
Tony <tonycarpenter@gmail.com>
2022-03-02 00:21:46 -0500

    Add welcome message and homepage buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import Navigation from './Navigation/Navigation'
+import HomeView from './HomeView/HomeView'
 
 // // const pickedStocks = async () => {
 // //   return 
@@ -30,20 +31,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HomeView />
       </div>
     );
   }
