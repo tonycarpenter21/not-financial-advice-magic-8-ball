dcfa2fa7a4910c1c53c7994847bb976a070fad05
Tony <tonycarpenter@gmail.com>
2022-03-03 13:33:27 -0500

    Refactor Page Not Found

--- src/PageNotFound/PageNotFound.js
+++ src/PageNotFound/PageNotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import './PageNotFound.css'
+import PageNotFoundGif from './PageNotFound.gif'
 
 const PageNotFound = () => {
   return(
     <div className="page-not-found-message">
+      <img src={PageNotFoundGif} alt="Page Not Found" className="not-found-gif"/>
       <div className="page-not-found-message-title">404 Error - Page Not Found</div>
       <div>Please return to the <Link to="/">Home Page</Link> or the <Link to="favorites">Favorites Page</Link></div>
     </div>
