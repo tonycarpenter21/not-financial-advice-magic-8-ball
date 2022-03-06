c45348134a079ad100fedd04192e1970f0a044f9
Tony <tonycarpenter@gmail.com>
2022-03-06 16:58:02 -0500

    Refactor JS and fix the Both Button glitch

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -23,23 +23,54 @@ class HomeView extends Component {
     this.props.addFavorite(newFavorite)
   }
 
+  createStockTip = () => {
+    let selectedStockTip = stockTips[Math.floor(Math.random() * stockTips.length)]
+    let selectedStock = stocks[0][Math.floor(Math.random() * stocks[0].length)].symbol
+    if (selectedStockTip.split('').includes('_') === true) {
+      let splitStockTip = selectedStockTip.split('_')
+      splitStockTip.splice(1, 0, selectedStock)
+      let finalStockTip = splitStockTip.join('')
+      return this.setState( {currentTip: finalStockTip} )
+    } else {
+      return this.setState( {currentTip: selectedStockTip} )
+    }
+  }
+
+  createCryptoTip = () => {
+    return this.setState( {currentTip: cryptoTips[Math.floor(Math.random() * cryptoTips.length)]} )
+  }
+
   randomTip = (inspirationType) => {
     let bothChoice = stockTips.concat(cryptoTips)
     if (inspirationType === "stock") {
-      let selectedStockTip = stockTips[Math.floor(Math.random() * stockTips.length)]
-      let selectedStock = stocks[0][Math.floor(Math.random() * stocks[0].length)].symbol
-      if (selectedStockTip.split('').includes('_') === true) {
-        let splitStockTip = selectedStockTip.split('_')
-        splitStockTip.splice(1, 0, selectedStock)
-        let finalStockTip = splitStockTip.join('')
-        this.setState( {currentTip: finalStockTip} )
-      } else {
-        this.setState( {currentTip: selectedStockTip} )
-      }
+      this.createStockTip()
     } else if (inspirationType === "crypto") {
-      this.setState( {currentTip: cryptoTips[Math.floor(Math.random() * cryptoTips.length)]} )
+      this.createCryptoTip()
     } else {
-      this.setState( {currentTip: bothChoice[Math.floor(Math.random() * bothChoice.length)]} )
+      Math.random() > .5 ? this.createStockTip() : this.createCryptoTip()
+    }
+  }
+
+  investmentTypeButtonMessage = () => {
+    return this.state.currentTip === "undefined" ? <p>Pick Your Investment Inspiration Type:</p> : <p>Click Again To Generate Another Idea:</p>
+  }
+
+  eightBallRender = () => {
+    if (this.state.currentTip === "undefined") {
+      return(
+        <div className="eight">8</div>
+      )
+    } else {
+      return(
+        <div className="window">
+          <div className="eight-ball-message">
+            {this.state.currentTip}
+            <Link to="/favorites">
+              <button className="favorite-button" name="favorite" onClick={event => this.handleClick(event)}>Favorite</button>
+            </Link>
+          </div>
+        </div>
+      )
     }
   }
 
@@ -56,25 +87,11 @@ class HomeView extends Component {
       </section>
       <section className="eight-ball-container">
         <div className="ball">
-          { this.state.currentTip === "undefined" ? (
-              <div className="eight">
-                8
-              </div>
-          ) : (
-              <div className="window ">
-                <div className="eight-ball-message">
-                  {this.state.currentTip}
-                  <Link to="/favorites">
-                    <button className="favorite-button" name="favorite" onClick={event => this.handleClick(event)}>Favorite</button>
-                  </Link>
-                </div>
-              </div>
-            )
-          }
+          {this.eightBallRender()}
         </div>
       </section>
       <section className="advice-type-container">
-        {this.state.currentTip === "undefined" ? <p>Pick Your Investment Inspiration Type:</p> : <p>Click Again To Generate Another Idea:</p>}
+        {this.investmentTypeButtonMessage()}
         <div className="advice-type-button-container">
           <button className="advice-type-button" name="stock" onClick={event => this.handleClick(event)}>Stocks</button>
           <button className="advice-type-button" name="both" onClick={event => this.handleClick(event)}>Both</button>
