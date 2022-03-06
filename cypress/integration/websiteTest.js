31653eb51af6be6c08a61ae320c3f49e90fb21ea
Tony <tonycarpenter@gmail.com>
2022-03-06 14:41:49 -0500

    Stub API call in Cypress Testing

--- cypress/integration/websiteTest.js
+++ cypress/integration/websiteTest.js
@@ -1,12 +1,52 @@
 describe('Load Website, check for elements, and check for functionality', () => {
-//   beforeEach( () => {
-//     cy.intercept('GET', 'https://api-address', {
-//       statusCode: 201,
-//       body: {"objectKeyName":[
-//         {example objects go here}
-//       ]}})
-//     .visit('http://localhost:3000') 
-//   });
+  beforeEach( () => {
+    cy.intercept('GET', 'https://yh-finance.p.rapidapi.com/stock/v2/get-recommendations?symbol=INTC', {
+      statusCode: 201,
+      body: 
+        {
+          "finance": {
+            "result": [
+                {
+                    "count": 1,
+                    "quotes": [
+                        {
+                            "language": "en-US",
+                            "region": "US",
+                            "quoteType": "EQUITY",
+                            "triggerable": true,
+                            "quoteSourceName": "Nasdaq Real Time Price",
+                            "postMarketPrice": 56.49,
+                            "postMarketChange": -0.0199966,
+                            "regularMarketChangePercent": 0.7847309,
+                            "sourceInterval": 15,
+                            "exchangeTimezoneName": "America/New_York",
+                            "exchangeTimezoneShortName": "EST",
+                            "gmtOffSetMilliseconds": -18000000,
+                            "market": "us_market",
+                            "exchange": "NMS",
+                            "regularMarketPreviousClose": 56.07,
+                            "postMarketChangePercent": -0.035386,
+                            "postMarketTime": 1646441982,
+                            "shortName": "Cisco Systems, Inc.",
+                            "fullExchangeName": "NasdaqGS",
+                            "esgPopulated": false,
+                            "tradeable": false,
+                            "marketState": "CLOSED",
+                            "regularMarketPrice": 56.51,
+                            "regularMarketTime": 1646427603,
+                            "regularMarketChange": 0.43999863,
+                            "exchangeDataDelayedBy": 0,
+                            "priceHint": 2,
+                            "symbol": "CSCO"
+                        },
+                    ]
+                }
+            ],
+            "error": null
+          }
+        }
+      })
+  });
 
   it('should display navigation bar', () => {
     cy.visit('http://localhost:3000/')
@@ -41,9 +81,13 @@ describe('Load Website, check for elements, and check for functionality', () => 
   })
 
   it('should display advice when button is clicked', () => {
-    cy.get('button.advice-type-button').contains('Both').click()
+    cy.get('button.advice-type-button').contains('Stocks').click()
     .get('div.eight-ball-message').should('be.visible')
   })
+
+  it('should display a stock from the API when button is clicked', () => {
+    cy.get('div.eight-ball-message').contains('CSCO')
+  })  
 
   it('should favorite investment idea when the favorite button is clicked and go to the Favorites page', () => {
     cy.get('button.favorite-button').click()
